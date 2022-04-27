4d283a59c70377eda793eeb751e8133021c48f84
mohamed rashad <m.rashad.sm@gmail.com>
2022-04-27 15:15:40 +0200

    editor scripts for create new event base

---
+++ Assets/Editor/CreateNewScriptFromCustomTemplate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+
+public class CreateNewScriptFromCustomTemplate
+{
+    private const string eventTemplatePath = "Assets/Editor/GameEvent.cs.txt";
+    private const string eventListenerTemplatePath = "Assets/Editor/GameEventListener.cs.txt";
+
+    [MenuItem(itemName: "Assets/Create/Class/Create Event Base", isValidateFunction: false, priority: 51)]
+    public static void CreateEventTemplate()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(eventTemplatePath, "GameEvent.cs");
+    }
+    [MenuItem(itemName: "Assets/Create/Class/Create Event Listener Base", isValidateFunction: false, priority: 51)]
+    public static void CreateEventListenerTemplate()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(eventListenerTemplatePath, "GameEventListener.cs");
+    }
+}
